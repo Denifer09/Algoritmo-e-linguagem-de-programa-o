f6813436850ad4e21bd65e0f2f60dd22b4a2598e
Denivaldo <110608377+Denivaldo-bit@users.noreply.github.com>
2022-10-05 22:10:43 -0300

    Add files via upload

---
+++ 7 ex lista1 calculo de xerox com valores diferente/7 exercicio/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double xn, xerox_inteira, xerox_desconto; 
+            
+
+            Console.WriteLine("digite o numero de xerox ");
+            xn = double.Parse(Console.ReadLine());
+
+
+            xerox_inteira = (xn * 0.50);
+
+            xerox_desconto = (xn * 0.30);
+
+            if (xn >= 200)
+            {
+                Console.WriteLine("o valor a ser pago é " + xerox_desconto);
+            }
+            else
+            {
+                Console.WriteLine("o valor a pagar é " + xerox_inteira);
+            }
+            
+
+           
+        }
+    }
+}
---
+++ 8 ex lista1 de mostrar a categoria de jogador/8 exercicio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.WriteLine("Digite a idade do jogador");
+
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade <= 13)
+            {
+                Console.WriteLine("o jogador é infantil");
+            }
+            else if (idade>=14 && idade <=17)
+            {
+                Console.WriteLine("o jogador é juvenil");
+            }
+            else if (idade>=18)
+            {
+                Console.WriteLine("o jogador é senior");
+            }
+
+
+        }
+    }
+}
